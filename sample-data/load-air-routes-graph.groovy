277a427b4913a3d2b047403db37b6252c2788caa
Kelvin Lawrence <gfxman@yahoo.com>
2017-10-07 14:23:54 -0500

    Add files via upload

---
+++ sample-data/load-air-routes-graph.groovy
@@ -0,0 +1,17 @@
+// You can use this file to load the air-routes graph from the Gremlin Console
+//
+// To execute use the console command ":load aload-air-routes-graph.groovy"
+//
+//
+//
+
+conf = new BaseConfiguration()
+conf.setProperty("gremlin.tinkergraph.vertexIdManager","LONG")
+conf.setProperty("gremlin.tinkergraph.edgeIdManager","LONG")
+graph = TinkerGraph.open(conf)
+
+// Change the path below to point to wherever you put the graphml file
+graph.io(graphml()).readGraph('/mydata/air-routes.graphml')
+
+g=graph.traversal()
+:set max-iteration 1000     
