f800db7de6df5a1d3baef5cd0f719e1d5cefced9
Kelvin Lawrence <gfxman@yahoo.com>
2017-11-26 11:09:47 -0600

    Add files via upload

--- sample-code/janus-inmemory.groovy
+++ sample-code/janus-inmemory.groovy
@@ -2,9 +2,15 @@
 // This is intended to be loaded and run inside the Gremlin Console from the Janus
 // Graph download.
 
+println "\n=====================================";[]
+println "Creating in-memory Janus Graph instance";[]
+println "=====================================\n";[]
 // Create a new graph instance
 graph = JanusGraphFactory.open('inmemory')
 
+println "\n===============";[]
+println "Defining labels";[]
+println "===============\n";[]
 // Define edge labels and usage
 mgmt = graph.openManagement()
 mgmt.makeEdgeLabel('route').multiplicity(MULTI).make()
@@ -19,6 +25,9 @@ mgmt.makeVertexLabel('country').make()
 mgmt.makeVertexLabel('continent').make()
 mgmt.commit()
 
+println "\n=============";[]
+println "Creating keys";[]
+println "=============\n";[]
 // Define vertex property keys
 mgmt = graph.openManagement()
 mgmt.makePropertyKey('code').dataType(String.class).cardinality(Cardinality.SINGLE).make()
@@ -39,7 +48,61 @@ mgmt = graph.openManagement()
 mgmt.makePropertyKey('dist').dataType(Integer.class).cardinality(Cardinality.SINGLE).make()
 mgmt.commit()
 
+println "\n==============";[]
+println "Building index";[]
+println "==============\n";[]
+
+// Construct a composite index for a few commonly used property keys
+graph.tx().rollback()
+mgmt=graph.openManagement()
+idx1=mgmt.buildIndex('airportIndex',Vertex.class)
+idx2 =mgmt.buildIndex('icaoIndex',Vertex.class)
+idx3 =mgmt.buildIndex('cityIndex',Vertex.class)
+iata = mgmt.getPropertyKey('code')
+icao = mgmt.getPropertyKey('icao')
+city = mgmt.getPropertyKey('city')
+idx1.addKey(iata).buildCompositeIndex()
+idx2.addKey(icao).buildCompositeIndex()
+idx3.addKey(city).buildCompositeIndex()
+mgmt.commit()
+
+
+println "\n=================================";[]
+println "Waiting for the index to be ready";[]
+println "=================================\n";[]
+
+mgmt.awaitGraphIndexStatus(graph, 'airportIndex').
+     status(SchemaStatus.REGISTERED).call()
+
+mgmt.awaitGraphIndexStatus(graph, 'icaoIndex').
+     status(SchemaStatus.REGISTERED).call()
+
+mgmt.awaitGraphIndexStatus(graph, 'cityIndex').
+     status(SchemaStatus.REGISTERED).call()
+
+// Once the index is created force a re-index
+println "\n===========";[]
+println "re-indexing";[]
+println "===========\n";[]
+mgmt = graph.openManagement()
+mgmt.awaitGraphIndexStatus(graph, 'airportIndex').call()
+mgmt.updateIndex(mgmt.getGraphIndex('airportIndex'), SchemaAction.REINDEX).get()
+mgmt.commit()
+
+mgmt = graph.openManagement()
+mgmt.awaitGraphIndexStatus(graph, 'icaoIndex').call()
+mgmt.updateIndex(mgmt.getGraphIndex('icaoIndex'), SchemaAction.REINDEX).get()
+mgmt.commit()
+
+mgmt = graph.openManagement()
+mgmt.awaitGraphIndexStatus(graph, 'cityIndex').call()
+mgmt.updateIndex(mgmt.getGraphIndex('cityIndex'), SchemaAction.REINDEX).get()
+mgmt.commit()
+
 // Load the air-routes graph
+println "\n========================";[]
+println "Loading air-routes graph";[]
+println "========================\n";[]
 graph.io(graphml()).readGraph('air-routes.graphml')
 graph.tx().commit()
 
@@ -51,4 +114,6 @@ mgmt.commit()
 
 // Setup our traversal object
 g = graph.traversal()
+
+
 
