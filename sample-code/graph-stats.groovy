6529ddec34645635f64ae6e4d75931fb3aefd9ac
Kelvin Lawrence <gfxman@yahoo.com>
2017-12-28 11:07:39 -0600

    Adding new sample to display some graph stats

---
+++ sample-code/graph-stats.groovy
@@ -0,0 +1,25 @@
+;[] // Small Groovy script that can be used from within the Gremlin console after
+;[] // the air-routes graph has been loaded that will provide statistics about the 
+;[] // graph. The use of ;[] is just to stop the console printing additional return values.
+
+println "\n\nA few statistics about the air-routes graph";[]
+println "===========================================";[]
+
+println "\nDistribution of vertices and edges";[]
+verts = g.V().groupCount().by(label).next();[]
+edges = g.E().groupCount().by(label).next();[]
+println "Vertices : ${verts}";[]
+println "Edges    : ${edges}";[]
+
+most = g.V().hasLabel('airport').order().by(out('route').count(),decr).limit(1).
+             project('ap','num','city').by('code').by(out('route').count()).by('city').next();[]
+
+println "\nThe airport with the most routes (incoming and outgoing) is ${most['ap']}/${most['city']} with ${most['num']}";[]
+
+longest = g.V().hasLabel('airport').values('longest').max().next();[]
+aptcity = g.V().has('longest',longest).valueMap('code','city').next();[]
+println "\nThe longest runway in the graph is ${longest} feet at ${aptcity['code'][0]}/${aptcity['city'][0]}";[]
+
+highest = g.V().hasLabel('airport').values('elev').max().next();[]
+aptcity = g.V().has('elev',highest).valueMap('code','city').next();[]
+println "\nThe highest airport in the graph is ${aptcity['code'][0]}/${aptcity['city'][0]} which is at ${highest} feet above sea level";[]
