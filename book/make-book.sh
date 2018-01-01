9e51c087555c80815e8213464890f78bb57a45f3
Kelvin Lawrence <gfxman@yahoo.com>
2018-01-01 10:48:42 -0600

    Now using asciidoctor-pdf to produce PDF output

--- book/make-book.sh
+++ book/make-book.sh
@@ -6,3 +6,5 @@ echo "*** Producing EPUB ***"
 pandoc -f docbook -t epub -N --number-sections --chapters --toc --toc-depth=4 Gremlin-Graph-Guide.xml -o Gremlin-Graph-Guide.epub
 echo "*** Producing MOBI ***"
 ebook-convert Gremlin-Graph-Guide.epub Gremlin-Graph-Guide.mobi
+echo "*** Producing PDF ***"
+asciidoctor-pdf Gremlin-Graph-Guide.adoc
