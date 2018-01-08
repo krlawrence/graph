0190afb81a9e345f2cc27063114230142fce443c
Kelvin Lawrence <gfxman@yahoo.com>
2018-01-08 14:56:06 -0600

    Added another Java sample

--- sample-code/GraphSearch3.java
+++ sample-code/GraphSearch3.java
@@ -1,12 +1,13 @@
-// GraphSearch2.java
+// GraphSearch3.java
 //
 // Simple example of using TinkerGraph with Java
+// This code builds upon the examples shown in GraphSearch and GraphSearch2
+// and adds the findLongestRoute method to the class.
 //
 // This example does the following:
 //   1. Create an empty TinkerGraph instance
 //   2. Load the air-routes graph
 //   3. Perform some searches against the graph
-//   4. Handle error conditions where graph elements are not found
 
 // I have highlighted any places where the Gremlin is slightly different from the 
 // Gremlin we can use in the Gremlin Console.
@@ -27,7 +28,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
-public class GraphSearch2 
+public class GraphSearch3 
 {
   private TinkerGraph tg;
   private GraphTraversalSource g;
@@ -55,13 +56,6 @@ public class GraphSearch2
   // If no route exists between the specified airports return -1
   public Integer getDistance(String from, String to)
   {
-    //The coalesce step avoids an exception when no result is found
-    //Integer d = (Integer) 
-    //  g.V().has("code",from).outE().as("edge").inV().has("code",to).
-    //        select("edge").by("dist").fold().
-    //        coalesce(__.unfold(),__.constant(-1)).next();
-
-    // Using toList() is another way to check for no result
     List result = 
       g.V().has("code",from).outE().as("edge").inV().has("code",to).
             select("edge").by("dist").toList();
@@ -71,34 +65,40 @@ public class GraphSearch2
     return(d);
   }
 
+  // Find the distance of the longest route in the graph
+  // Returns a map with keys of from,distance and to
+  public Map findLongestRoute()
+  {
+    // Note how we need to prefix certain things that we would not have to
+    // when using the Gremlin console with "__." and "Order."
+    Map result = 
+      g.V().hasLabel("airport").outE("route").
+            order().by("dist",Order.decr).limit(1).
+            project("from","distance","to").
+            by(__.inV().values("code")).by("dist").by(__.outV().values("code")).next();
+
+    return(result);
+  }
+  
+
   // Run some tests
   public static void main(String[] args) 
   {
     // If you want to check your Gremlin version, uncomment the next line
     //System.out.println("Gremlin version is: " + Gremlin.version());
 
-    GraphSearch2 gs = new GraphSearch2();
+    GraphSearch3 gs = new GraphSearch3();
     boolean loaded = gs.loadGraph("air-routes.graphml");
 
-    // The data below contains a non existent route between XXX and YYY to test the
-    // error handling when a query returns no data.
     if (loaded)
     {
-      String[][] places = {{"AUS","LHR"},{"JFK","PHX"},{"SYD","LAX"},
-                           {"PEK","HND"},{"HKG","MEL"},{"MIA","SFO"},
-                           {"MNL","BKK"},{"XXX","YYY"},{"DOH","JNB"},
-                           {"NRT","FRA"},{"AMS","GVA"},{"CDG","SIN"}};
+      Map longest = gs.findLongestRoute() ;
+      String s = "The longest route in the graph is between " + longest.get("from");
+      s += " and " + longest.get("to") + " covering a distance of ";
+      s += longest.get("distance") + " miles." ;
 
-      System.out.println("\n\nFrom    To   Distance");
-      System.out.println("=====================");
+      System.out.println(s);
 
-      Integer dist;
-      
-      for (String[] p : places)
-      {
-        dist = gs.getDistance(p[0],p[1]);
-        System.out.format("%4s   %4s   %5d\n",p[0],p[1],dist) ;
-      }  
     }
   }      
 }
