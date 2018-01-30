e9a7d8f784c75b804ea01aecaf87bcbce8a88b53
Kelvin Lawrence <gfxman@yahoo.com>
2018-01-30 15:09:51 -0600

    More examples of Enum usage

--- sample-code/TestImports.java
+++ sample-code/TestImports.java
@@ -15,6 +15,8 @@ import org.apache.tinkerpop.gremlin.process.traversal.Path;
 import org.apache.tinkerpop.gremlin.process.traversal.*;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.Property;
+import org.apache.tinkerpop.gremlin.structure.Column;
 import org.apache.tinkerpop.gremlin.structure.io.IoCore;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.*;
 import org.apache.tinkerpop.gremlin.structure.T;
@@ -47,34 +49,112 @@ public class TestImports
     return true;
   }
   
+  // Help make the output easier to read
+
+  private void showHeader(String str)
+  {
+    String tmp = "***** " +str + " *****" ;
+    String tmp2 = "" ;
+
+    for (int i=0; i<tmp.length();i++) {tmp2 += "*";}
+    System.out.println(tmp2);
+    System.out.println(tmp);
+    System.out.println(tmp2+"\n");
+  }
+
   // Demonstrate how to use common Gremlin constructs from Java
   public void runTests()
   {
-
+    // ---------------------------------------------------
     // Examples of using local scope and order with a list
+    // ---------------------------------------------------
     List results;
+
+    showHeader("Scope local order");
 
     results = g.V().has("region","US-TX").values("code").fold().
                     order(Scope.local).toList();
 
     System.out.println(results + "\n");
 
+    showHeader("Scope.local Order.decr");
     results = g.V().has("region","US-TX").values("code").fold().
                     order(Scope.local).by(Order.decr).toList();
 
     System.out.println(results + "\n");
 
+
+    // -------------------------
+    // More examples of ordering
+    // -------------------------
+    
+    showHeader("Order.shuffle");
+
+    for (int i=0; i<3; i++)
+    {
+      results = g.V().has("region","US-TX").values("code").
+                      order().by(Order.shuffle).toList();
+
+      System.out.println(results + "\n");
+    }
+
+    showHeader("Order.decr");
+
+    results = g.V().has("region","US-TX").values("code").
+                    order().by(Order.decr).toList();
+
+    System.out.println(results + "\n");
+
+
+    // ----------------------------
     // Examples of using predicates
+    // ----------------------------
+
+    
+    showHeader("Predicates Texas cities neq('Dallas')");
 
     results = g.V().has("region","US-TX").has("city", P.neq("Dallas")).
                     values("city").dedup().fold().toList();
 
     System.out.println(results + "\n");
 
+    showHeader("Predicates Texas cities without('Dallas','Houston','Austin')");
+    
     results = g.V().has("region","US-TX").
                     has("city",P.without("Dallas","Houston","Austin")).
                     values("city").dedup().fold().toList();
 
+    System.out.println(results + "\n");
+
+
+    // --------------------------------------------------
+    // Selecting keys and values from maps and properties
+    // --------------------------------------------------
+
+    showHeader("Column.keys Austin vertex");
+    
+    results = g.V().has("airport","code","AUS").valueMap().select(Column.keys).toList();
+    System.out.println(results + "\n");
+
+    showHeader("Column.values Austin vertex");
+
+    results = g.V().has("airport","code","AUS").valueMap().select(Column.values).toList();
+    System.out.println(results + "\n");
+
+    List<?> props =  
+        g.V().has("code","SFO").properties().order().by(T.key).toList();
+
+    showHeader("Order properties by(T.key)");
+    props.forEach((p) -> System.out.println(p));
+    System.out.println();
+
+    // --------------------
+    // Working with labels.
+    // --------------------
+    
+    showHeader("groupCount().by(T.label)");
+
+    results = g.V().groupCount().by(T.label).toList();
     System.out.println(results + "\n");
   }
 
