5dfcbda5752789555081e717863c7a41fc39afbe
Kelvin Lawrence <gfxman@yahoo.com>
2018-12-09 12:13:06 -0600

    Add a few more sample queries to the example

--- sample-code/quick-social2.groovy
+++ sample-code/quick-social2.groovy
@@ -25,10 +25,12 @@ g.addV("person").property("name","Fred").as("fred").
   addV("place").property("name","Miami").as("mia").
   addE("knows").from("bill").to("fred").
   addE("knows").from("bill").to("max").
+  addE("knows").from("bill").to("peter").
   addE("lives_in").from("bill").to("mia").
   addE("knows").from("fred").to("mary").
   addE("knows").from("fred").to("janet").
   addE("knows").from("fred").to("bill").
+  addE("knows").from("fred").to("max").
   addE("lives_in").from("fred").to("sea").
   addE("knows").from("janet").to("fred").
   addE("knows").from("janet").to("lily").
@@ -40,9 +42,11 @@ g.addV("person").property("name","Fred").as("fred").
   addE("knows").from("mary").to("fred").
   addE("lives_in").from("mary").to("nyc").
   addE("knows").from("max").to("bill").
+  addE("knows").from("max").to("fred").
   addE("knows").from("max").to("mary").
   addE("knows").from("max").to("peter").
   addE("lives_in").from("max").to("bos").
+  addE("knows").from("peter").to("bill").
   addE("knows").from("peter").to("susan").
   addE("knows").from("peter").to("max").
   addE("lives_in").from("peter").to("dal").
@@ -53,11 +57,40 @@ g.addV("person").property("name","Fred").as("fred").
 // What does the graph look like?
 g.V().order().by('name').outE().inV().path().by('name').by(label)
 
-g.V().out().groupCount().by('name')
-g.V().in().groupCount().by('name')
+// What is the distribution of relationships?
+g.V().hasLabel('person','place').out().groupCount().by('name')
+g.V().hasLabel('person','place').in().groupCount().by('name')
+
+
+// What vertices do we have?
+g.V().order().by('name').outE().inV().path().by('name').by(label) 
 
 // Who does Max know that lives in Miami?
  g.V().has('name','Max').out('knows').where(out('lives_in').values('name').is('Miami')).values('name')
 
 // Who lives in New York City?
 g.V().hasLabel('person').where(out('lives_in').has('name','New York City')).values('name')
+
+// Who does Mary know that already know each other?
+g.V().has('person','name','Mary').out('knows').as('x').aggregate('a').
+      out('knows').where(within('a')).path().by('name').from('x')
+     
+// Who does Mary know whose friends don't know Mary?
+g.V().has('person','name','Mary').as('mary').
+     out('knows').aggregate('maryalreadyknows').
+     out('knows').where(neq('mary')).where(without('maryalreadyknows')).
+     values('name').dedup()
+   
+// Who does Mary know whose friends don't know Mary (using match) ?
+g.V().has('person','name','Mary').
+      match(__.as('a').out('knows').as('b')
+           ,__.as('b').out('knows').where(neq('a')).as('c')
+           ,__.not(__.as('a').out().as('c'))).
+      select('c').by('name').dedup()
+
+
+
+            
+          
+
+
