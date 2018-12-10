11f0b0a24a47f3f1c7f7493efda7695df511eceb
Kelvin Lawrence <gfxman@yahoo.com>
2018-12-10 10:20:08 -0600

    Very minor tidy up

--- sample-code/quick-social2.groovy
+++ sample-code/quick-social2.groovy
@@ -5,25 +5,25 @@
 // included. This will work unchanged from the Gremlin console if
 // using a TinkerGraph or with any other TinkerPop graph database if
 // you remove the first two lines and the comment lines.
-
+ 
 // Remove these two lines if using a graph db other than TinkerGraph.
 graph=TinkerGraph.open()
 g=graph.traversal()
-
-g.addV("person").property("name","Fred").as("fred").
-  addV("person").property("name","Mary").as("mary").
-  addV("person").property("name","Albert").as("albert").
+ 
+g.addV("person").property("name","Albert").as("albert").
   addV("person").property("name","Bill").as("bill").
+  addV("person").property("name","Fred").as("fred").
   addV("person").property("name","Janet").as("janet").
-  addV("person").property("name","Lily").as("lily").
+  addV("person").property("name","Mary").as("mary").
   addV("person").property("name","Max").as("max").
-  addV("person").property("name","Susan").as("susan").
+  addV("person").property("name","Lily").as("lily").
   addV("person").property("name","Peter").as("peter").
-  addV("place").property("name","New York City").as("nyc").
+  addV("person").property("name","Susan").as("susan").
   addV("place").property("name","Boston").as("bos").
   addV("place").property("name","Dallas").as("dal").
-  addV("place").property("name","Seattle").as("sea").
   addV("place").property("name","Miami").as("mia").
+  addV("place").property("name","New York City").as("nyc").
+  addV("place").property("name","Seattle").as("sea").
   addE("knows").from("albert").to("mary").
   addE("lives_in").from("albert").to("mia").
   addE("knows").from("bill").to("fred").
@@ -57,54 +57,53 @@ g.addV("person").property("name","Fred").as("fred").
   addE("knows").from("susan").to("peter").
   addE("knows").from("susan").to("mary").
   addE("lives_in").from("susan").to("sea").iterate()
-
+ 
 // What does the graph look like?
 g.V().order().by('name').outE().inV().path().by('name').by(label)
-
+ 
 // What is the distribution of relationships?
 g.V().hasLabel('person','place').out().groupCount().by('name')
 g.V().hasLabel('person','place').in().groupCount().by('name')
-
+ 
 // What vertices do we have?
 g.V().order().by('name').outE().inV().path().by('name').by(label) 
-
+ 
 // Who does Max know that lives in Miami?
  g.V().has('name','Max').out('knows').where(out('lives_in').values('name').is('Miami')).values('name')
-
+ 
 // Who lives in New York City?
 g.V().hasLabel('person').where(out('lives_in').has('name','New York City')).values('name')
-
+ 
 // Who are Mary's friends , friends ?
 g.V().has('person','name','Mary').as('mary').
       out('knows').out('knows').where(neq('mary')).dedup().values('name')
-
+ 
 // Who are Mary's friends , friends ? (using match) 
 g.V().has('person','name','Mary').
       match(__.as('a').out('knows').as('b')
            ,__.as('b').out('knows').where(neq('a')).as('c')).
       select('c').by('name').dedup() 
-
+ 
 // Who does Mary know that already know each other?
 g.V().has('person','name','Mary').out('knows').as('x').aggregate('maryalreadyknows').
       out('knows').where(within('maryalreadyknows')).path().by('name').from('x')
-     
+      
 // Who does Mary know whose friends don't know Mary?
 g.V().has('person','name','Mary').as('mary').
      out('knows').aggregate('maryalreadyknows').
      out('knows').where(neq('mary')).where(without('maryalreadyknows')).
      values('name').dedup()
-   
+    
 // Who does Mary know whose friends don't know Mary? (using match)
 g.V().has('person','name','Mary').
       match(__.as('mary').out('knows').as('maryalreadyknows')
            ,__.as('maryalreadyknows').out('knows').where(neq('mary')).as('c')
            ,__.not(__.as('mary').out().as('c'))).
       select('c').by('name').dedup()
-
-// Which of Mary's friends already know each other and which do not?
- g.V().has('name','Mary').as('mary').out('knows').aggregate('maryknows').
-       local(union(identity().values('name'),
-             out('knows').where(neq('mary')).where(within('maryknows')).
-       values('name')).fold())
-
+ 
+// Which of Mary's friends already know each other and which do not?  
+g.V().has('name','Mary').as('mary').out('knows').aggregate('maryknows').
+      local(union(identity().values('name'),
+            out('knows').where(neq('mary')).where(within('maryknows')).
+      values('name')).fold())
 
