5bb793ce8de8a8fdcd5feb070cb4fdf9dda55ed5
Kelvin Lawrence <gfxman@yahoo.com>
2019-03-16 10:27:27 -0500

    Add an example of connecting to a Gremlin server from Node.js #107

---
+++ sample-code/glv-client.js
@@ -0,0 +1,71 @@
+// glv-client.js
+//
+// Example of using Gremlin from Node.js via a remote connection.
+//
+// This example assumes you have previously used NPM as follows to install Gremlin support:
+//
+//    npm install gremlin
+
+
+// Import the Gremlin specific objects we might need to reference
+const gremlin = require('gremlin');
+const Graph = gremlin.structure.Graph;
+const __ = gremlin.process.statics;
+const { t: { id }, order,cardinality,column,scope } = gremlin.process;
+
+// Note that if we just wanted to import 'decr' rather than all of order we could do:
+// const { order: { decr } } = gremlin.process;
+
+const hostname = 'localhost'
+const port=8182
+
+console.log("Creating connection");
+wspath = `ws://${hostname}:${port}/gremlin`;
+
+// NOTE: The empty object {} is to work around a bug in the 
+//       Gremlin JavaScript 3.3.5 and 3.4 clients.
+const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
+const connection = new DriverRemoteConnection(wspath,{});
+const graph = new Graph();
+console.log("Connecting to :" + wspath);
+const g = graph.traversal().withRemote(connection);
+console.log("Connection created");
+
+// Run a few tests that use the objects we imported above
+async function runTests() {
+  try {
+    const texas = await
+      g.V().has('region','US-TX').
+            values('city').
+            order().by(order.decr).
+            toList();
+      console.log("Airports in Texas");      
+      console.log(texas);
+
+    const runways = await
+      g.V().has('region','US-TX').
+            group().by('code').by(__.out().count()).
+            order(scope.local).by(column.values,order.decr).
+            toList();
+      console.log("Route counts for airports in Texas");      
+      console.log(runways);
+  } catch(e) {
+      console.error(`Something went wrong:\n ${e}`);
+  } finally {
+      connection.close();
+  }
+}
+
+(async function() {
+  try {
+    console.log("Application starting");
+    await runTests();
+    console.log("Tests complete");
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}());
+
+
