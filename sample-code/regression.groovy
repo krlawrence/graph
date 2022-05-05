55cf67ca82422840de6b915044305ca6db16f4c0
Kelvin Lawrence <gfxman@yahoo.com>
2022-05-04 19:36:05 -0500

    Include 3.6.x in allowed versions

--- sample-code/regression.groovy
+++ sample-code/regression.groovy
@@ -24,7 +24,8 @@ println "Reported version is $ver";[]
 
 assert ver[0]=='3' && ((ver[2]=='3' && ver[4].toInteger() >= 1) 
        || (ver[2]=='4' && ver[4].toInteger() >= 0)
-       || (ver[2]=='5' && ver[4].toInteger() >= 0));[]
+       || (ver[2]=='5' && ver[4].toInteger() >= 0))
+       || (ver[2]=='6' && ver[4].toInteger() >= 0));[]
 
 n=status( "Checking air-routes version",n);[]
 ver=g.V().hasLabel('version').values('code').next();[]
