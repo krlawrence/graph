c2ca9fbcd67e3deea85c9b9c98cd3131ab3776dc
Stephen Mallette <stepmall@amazon.com>
2026-01-12 13:40:12 -0500

    Minor fix to book build syntax to get a final publication.

--- book/make-book.sh
+++ book/make-book.sh
@@ -5,7 +5,7 @@ asciidoctor -n -b docbook -d book Gremlin-Graph-Guide.adoc -o krltemp.xml
 sed -e s/language=\"groovy\"/language=\"java\"/ krltemp.xml > Gremlin-Graph-Guide.xml
 rm krltemp.xml
 echo "*** Producing EPUB ***"
-pandoc -f docbook -t epub -N --number-sections --chapters --toc --toc-depth=4 Gremlin-Graph-Guide.xml -o Gremlin-Graph-Guide.epub
+pandoc -f docbook -t epub -N --number-sections --top-level-division=chapter --toc --toc-depth=4 Gremlin-Graph-Guide.xml -o Gremlin-Graph-Guide.epub
 echo "*** Producing MOBI ***"
 ebook-convert Gremlin-Graph-Guide.epub Gremlin-Graph-Guide.mobi
 echo "*** Producing PDF ***"
